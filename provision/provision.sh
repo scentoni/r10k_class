8bffc63188b2719734dfd25483a1aca22291b210
cbarbour <cbarbour@taos.com>
2014-03-20 00:31:17 -0700

    Ensure install to /etc/puppet/modules

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,5 +3,5 @@
 sudo yum install -y https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 sudo yum install -y puppet-server
 
-sudo puppet module install puppetlabs-inifile
-sudo puppet module install zack-r10k
+sudo puppet module install puppetlabs-inifile --modulepath=/etc/puppet/modules:/usr/share/puppet/modules
+sudo puppet module install zack-r10k --modulepath=/etc/puppet/modules:/usr/share/puppet/modules
