2843833c1d7859ff8cd92cdfa0ec371d976d476b
Chris Barbour <cbarbour@taos.com>
2014-03-21 23:46:36 -0700

    Roles & profiles initial commit

---
+++ profiles/manifests/base.pp
@@ -0,0 +1,29 @@
+class profiles::base {
+  $groups = hiera_hash('group', undef)
+  if $groups {
+    create_resources('@group', $groups)
+  }
+  Group <| tag == 'base' |>
+
+  $accounts = hiera_hash('account', undef)
+  if $accounts {
+    create_resources('@account', $accounts)
+  }
+  Account <| tag == 'base' |>
+
+  $yumrepos = hiera_hash('yumrepo', undef)
+  if $yumrepos {
+    create_resources('@yumrepo', $yumrepos)
+  }
+  Yumrepo <| tag == 'base' |>
+  Yumrepo <| tag == "${osfamily}-${operatingsystemmajrelease}" |>
+  Yumrepo <| tag == "${osfamily}-${operatingsystemrelease}" |>
+  Yumrepo <| tag == "${operatingsystem}-${operatingsystemmajrelease}" |>
+  Yumrepo <| tag == "${operatingsystem}-${operatingsysterelease}" |>
+
+  $packages = hiera_hash('package', undef)
+  if $packages {
+    create_resource('@package', $packages)
+  }
+  Package <| tag == 'base' |>
+}
