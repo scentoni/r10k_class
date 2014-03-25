6bf083e0a1577e2c6f951fd8fdf71b090687f94f
Chris Barbour <cbarbour@taos.com>
2014-03-24 20:51:16 -0700

    Added packages to provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -4,7 +4,12 @@ sudo yum update -y
 
 sudo yum install -y https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 sudo yum install -y puppet puppet-server
+
 sudo yum install -y ruby ruby-devel rubygems
+sudo yum install -y gcc make #Required to build gems
+
+sudo yum install -y git
+sudo yum install -y man
 
 sudo gem install rubygems-update
 sudo update_rubygems
